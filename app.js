d2e303b627e02c1ebcd65fcced97d8832a98c846
Claire Lesage <clairellesage@gmail.com>
2017-04-08 13:48:24 +0000

    server runs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+var Chart = require('chart.js')
+const createChart = require('./javascripts/chart.js')
 app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', createChart)
 });
 
 app.listen(8080, function() {
