400e1a3c1119af7be9716128fc4aa1d7b572a33f
Claire Lesage <clairellesage@gmail.com>
2017-04-09 20:14:39 +0000

    using http library

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ This repo serves as a template for Milieu's reports. There are three goals in mi
 
 - for any new client we can simply plug their .csv file into watson
 - the watson-parser can take any question (column) and retrieve the keywords
-- we can then pick which graphical representation best fits the data
+- put the data back into a .csv file
+
+We can then pick which graphical representation best fits the data using libraries or working with excel graphs.
--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-const express = require('express');
-const app = express();
-var Chart = require('chart.js')
-const createChart = require('./javascripts/chart.js')
-app.set("view engine", "ejs");
+var request = require('request')
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var client = new XMLHttpRequest()
+var base64 = require('base64-js').fromByteArray;
 
-app.get('/', function (req, res) {
-  res.render('index', createChart)
-});
+client.open("GET", "http://twitter.com/account/verify_credentials.json", false, "TestAct", "password");
+client.setRequestHeader("Authorization", "Basic " + base64("63c49a5a-11dc-45e6-9451-eb03743044b0': 'fAlSUZYF5VZI"));
+client.send(null);
 
-app.listen(8080, function() {
-	console.log('Listening on port 8080!')
-});
+ let parameters = {
+   'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
+   'features': {
+     'entities': {
+       'emotion': true,
+       'sentiment': true,
+       'limit': 2
+     },
+     'keywords': {
+       'emotion': true,
+       'sentiment': true,
+       'limit': 2
+     }
+   }
+ }
+
+  request.post('https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(parameters)
+  }, function (err, res, body) {
+    if (err) {
+      console.log("error")
+    } else {
+      console.log(body)
+    }
+  });
+
+  // a POST request will be sent to http://www.mockbin.com
+  // with body an application/x-www-form-urlencoded body:
+  // foo=bar&hello=world
