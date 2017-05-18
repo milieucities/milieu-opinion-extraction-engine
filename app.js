0071de76953976efb39e9228b36086c64f145236
Claire Lesage <clairellesage@gmail.com>
2017-05-18 15:35:23 -0400

    not working code for sake of PR

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ const fs = require('fs');
 const csvToJson = require('csvtojson')
 const Converter = require("csvtojson").Converter;
 const csvConverter = new Converter({});
+
 //user provides csv path at command line
 const fileName = process.argv[2];
 const csv = require(`./${fileName}`);
+
 // const json = require(`./${fileName}.json`)
 //user provides question number or text in quotation marks at command line
 const questionText = process.argv[3];
+
 //watson credentials
 const username = require('./test-code/keys.js').username;
 const password = require('./test-code/keys.js').password;
+
 //watson only runs for this amount of comments to save API calls.
 //to analyze all comments, change this to txt.length
 const apiCalls = 4;
+
 //responses get pushed into this array to be analyzed
 const analysis = [];
+
 //required params for watson API, see documentation for more options
 const parameters = {
     'text': '',
@@ -38,21 +44,28 @@ function main(csv) {
   function createJSON(csv) {
     //end_parsed will be emitted once parsing finished
     csvConverter.on("end_parsed", function(jsonObj) {
-      fs.writeFile('${fileName}.json', JSON.stringify(jsonObj, null, "  "), function (err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log('Success! Check ${fileName}.json!');
+      try {
+          fs.writeFile('${fileName}.json', JSON.stringify(jsonObj, null, "  "), function (err) {
+          try {
+            return; 
+          }
+          catch(err) {
+            console.log(`Error: ${err}. Unable to parse the csv file.`) 
+          }
       })
+      } catch (err) {
+        console.log(`Error: ${err}. Unable to parse the csv file.`)
+      }
     });
     //read from csv file
-    fs.createReadStream(csvFilePath).pipe(csvConverter);
+    fs.createReadStream(fileName).pipe(csvConverter);
   }
 
   //this function needs access to the output file from createJSON, otherwise it gets an unexpected identifier error from csv
   function getColumns(json) {
     //trying to get the .json file...
     const jsonFile = createJSON(csv)
+    console.log(jsonFile)
     for (i = 0; i < apiCalls; i++) {
       if (jsonFile[i][questionText] != null) {
         var participantAnswer = jsonFile[i][questionText]
