0f3a5489982af28106837b7ed4067243696080c9
Claire Lesage <clairellesage@gmail.com>
2017-05-29 14:36:01 -0400

    works with promises

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const csvConverter = new Converter({});
 
 //user provides csv path at command line
 const fileName = process.argv[2];
-// const csv = require(`./${fileName}`);
 
 //user provides question number or text in quotation marks at command line
 const questionText = 'List up to three challenges related to the existing Fredericton trails/bikeway system:'
@@ -18,9 +17,10 @@ const password = require('./dev/keys.js').password;
 
 //watson only runs for this amount of comments to save API calls.
 //to analyze all comments, change this to txt.length
-const apiCalls = 4;
+const apiCalls = 5;
 
 //responses get pushed into this array to be analyzed
+const comments = [];
 const analysis = [];
 
 //required params for watson API, see documentation for more options
@@ -42,21 +42,21 @@ function main(filename) {
   parseCSV(filename)
   .then(function(json) {
     return getColumns(json);
-  }).then(function(parameters){
-    return analyzeWatson(parameters);
   }).then(function(analysis){
-    console.log(writeToJSON(analysis))
+    return analyzeWatson(analysis);
+  }).then(function(analysis){
+    return writeToJSON(analysis);
   })
 }
 
 main(fileName);
 
-function parseCSV (filename) {
+function parseCSV(filename) {
   return new Promise(function(resolve, reject) {
     var converter = new Converter({});
     converter.on("end_parsed", function(json, err) {
       if (err) {
-        reject(new Error("Cannot parse"))
+        reject(new Error("Cannot parse CSV!"))
       }
       resolve(json);
     });
@@ -64,51 +64,50 @@ function parseCSV (filename) {
   });
 }
 
-  //this function needs access to the output file from createJSON, otherwise it gets an unexpected identifier error from csv
 function getColumns(json) {
   return new Promise(function(resolve, reject){
-    for (i = 1; i < apiCalls; i++) {
-      if (json[i][questionText] != '') {
-        var participantAnswer = json[i][questionText];
+    for (i = 1; i <= apiCalls; i++) {
+      var participantAnswer = json[i][questionText];
+      if (participantAnswer != '') {
         parameters.text = participantAnswer;
-        resolve(parameters);
-      } else {
-        reject(new Error("Not a valid column, please check the CSV file"));
+        comments.push(parameters.text)
+      } 
+    }
+    resolve(comments);
+  })
+}
+
+//NOTE: each params needs to be pushed in to analysis array, to analyze all comments
+
+function analyzeWatson(comments) {
+    return new Promise(function(resolve, reject) {
+      comments.forEach(function(comment) { 
+        parameters.text = comment;
+        var request = new XMLHttpRequest();
+        request.open('POST', url, false, username, password);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(parameters));
+        if (request.status === 200) {
+          var response = JSON.parse(request.responseText);
+          analysis.push(response)
+        } else {
+      reject(new Error(`An http error occurred; ${JSON.stringify(parameters)}, ${request.status}, ${request.responseText}, ${request.error}` ));
       }
-    };
+    })
+    resolve(analysis)
   })
 }
-
-//JSON.stringify(analysis, null, "  ")
-
-    
-    // var response = analyzeWatson(parameters);
-    // if (response) {
-    //   analysis.push(response);
-
-//  return writeToJSON(JSON.stringify(analysis, null, "  ")); 
-
-function analyzeWatson(parameters) {
-  return new Promise(function(resolve, reject) {
-    var request = new XMLHttpRequest();
-    request.open('POST', url, false, username, password);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify(parameters));
-    if (request.status === 200) {
-      resolve(JSON.parse(request.responseText));
-    } 
-    reject(new Error(`An http error occurred; ${JSON.stringify(parameters)}, ${request.status}, ${request.responseText}, ${request.error}` ));
-  })
-}
-
 
 //changed body to stringified body
 function writeToJSON(analysis) {
   return new Promise(function(resolve, reject) {
     fs.writeFile(`analysis-${fileName}`, JSON.stringify(analysis, null, "  "), function (err) {
+      if (err) {
+        reject(new Error(`Cannot write output file.`))
+      }
       resolve(console.log(`Success! Check analysis-${fileName}`));
     });
-    reject(new Error(`Cannot write output file.`))
+
   })
 }
 
