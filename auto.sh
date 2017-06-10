bf32db615c9878459999af3a4b0133ec0309168e
Ilie Diacov <iliadiacov@mail.ru>
2017-06-10 00:03:27 -0400

    edit shell script

--- auto.sh
+++ auto.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # system_page - A script to produce a system information HTML file
+
+#VARS
+
+#
 launchMOEE=$ node $1 $2 $3 $4
 
 
@@ -9,15 +13,15 @@ cd ~/projects/milieu-opinion-extraction-engine
 # Launch MOEE
 echo $launchMOEE
 echo analysis-$2
-
 # Transform CSV into JSON
-# cat analysis-$2 > analysis$3_$2[$4].json
+cat analysis-$2 > analysis$3_$2[$4].json
 # Move analyzed results to KILN repository
-# mv ~/projects/milieu-opinion-extraction-engine/analysis$3_$2[$4].json ~/projects/the-kiln/data/
+mv ~/projects/milieu-opinion-extraction-engine/analysis$3_$2[$4].json ~/projects/the-kiln/data/
 # Go to the KILN repository
-# cd ~/projects/the-kiln/
+cd ~/projects/the-kiln/
 
-#node demographics.js ./data/analysis$3_$2[$4].json
+
+node demographics.js ./data/analysis$3_$2[$4].json
 
 #ls -al
 #if $3 = -d
