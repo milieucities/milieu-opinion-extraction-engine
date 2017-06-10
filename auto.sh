ad46d4ad2a09de4007fb8796b868786470a3b46f
Ilie Diacov <iliadiacov@mail.ru>
2017-06-09 23:49:36 -0400

    add Shell script

---
+++ auto.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# system_page - A script to produce a system information HTML file
+launchMOEE=$ node $1 $2 $3 $4
+
+
+#Go to the MOEE repository
+cd ~/projects/milieu-opinion-extraction-engine
+# Launch MOEE
+echo $launchMOEE
+
+# echo analysis-$2
+# Transform CSV into JSON
+# cat analysis-$2 > analysis$3_$2[$4].json
+# Move analyzed results to KILN repository
+# mv ~/projects/milieu-opinion-extraction-engine/analysis$3_$2[$4].json ~/projects/the-kiln/data/
+# Go to the KILN repository
+# cd ~/projects/the-kiln/
+
+#node demographics.js ./data/analysis$3_$2[$4].json
+
+#ls -al
+#if $3 = -d
+#then
+
+
+
+#
+#
+#
+# Current directory
+#  echo $(pwd)
