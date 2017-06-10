944c7e1a5571eff73f5468ba89055eb9f5fe13fe
Ilie Diacov <iliadiacov@mail.ru>
2017-06-10 00:15:36 -0400

    simplify shell script

--- demo.sh
+++ demo.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# system_page - A script to produce a system information HTML file
+# demo.sh - A script to produce analyzed and calculated demographics JSON file.
 
 #VARS
-
-#
+# Simplified MOEE launch
 launchMOEE=$ node app.js $1 -d $2
-
+# MOEE output file
+moeeOutput= echo analysis-$1
 
 #Go to the MOEE repository
 cd ~/projects/milieu-opinion-extraction-engine
 # Launch MOEE
 echo $launchMOEE
-echo analysis-$1
 # Transform CSV into JSON
 cat analysis-$1 > analysis-d_$1[$2].json
 # Move analyzed results to KILN repository
@@ -20,7 +19,7 @@ mv ~/projects/milieu-opinion-extraction-engine/analysis-d_$1[$2].json ~/projects
 # Go to the KILN repository
 cd ~/projects/the-kiln/
 # Conduct demographics calculations
-node demographics.js ./data/analysis-d_$1[$2].json
+node demographics.js ./data/calculated-demo_$1[$2].json
 
 #ls -al
 #if -d = -d
